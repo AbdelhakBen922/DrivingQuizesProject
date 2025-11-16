47eade44e25593ee8efdfb6dc97770e530b8bf45
AbdelhakBen922 <abdelhak.benbouziane@ensia.edu.dz>
2025-11-16 17:42:54 +0100

    add ing quiz page v1

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("select-quiz","routes/Quiz/quiz-select.tsx"),
+    route("quiz/:quizId","routes/Quiz/quiz.tsx"),
 ] satisfies RouteConfig;
